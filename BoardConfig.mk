82b0db05fa49fb4806b685c918d78a195681528d
Uma Mehta <umamehta@codeaurora.org>
2016-09-30 15:32:08 +0530

    msm8953_32: Set absolute path for seccomp
    changes to pick seccomp folder for 8953_32.
    
    Change-Id: I415ad6e3cafb03ad3d3bf38e3c8ddc4210698cd9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,7 +85,7 @@ BOARD_PERSISTIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlycon=msm_hsl_uart,0x78af000
 #BOARD_KERNEL_SEPARATED_DT := true
 
-BOARD_SECCOMP_POLICY := device/qcom/$(TARGET_BOARD_PLATFORM)/seccomp
+BOARD_SECCOMP_POLICY := device/qcom/msm8953_32/seccomp
 
 BOARD_EGL_CFG := device/qcom/msm8953_32/egl.cfg
 
