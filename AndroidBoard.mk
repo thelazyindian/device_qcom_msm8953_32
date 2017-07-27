780e1c2eb06d52a4f1478b15d8bc80c840b978a9
Ajit Vaishya <ajitv@codeaurora.org>
2017-07-27 12:29:17 +0530

    Point Wi-Fi overlay configurations to /vendor/etc/wifi
    Point wpa_supplicant_overlay.conf and p2p_supplicant_overlay.conf
    file locations to /vendor/etc/wifi
    
    CRs-Fixed: 2052942
    Change-Id: I686b2b83c5a5b76adf75a7c3c5676b573e2d5832

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -139,7 +139,7 @@ LOCAL_MODULE       := wpa_supplicant_overlay.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
+LOCAL_MODULE_PATH  := $(PRODUCT_OUT)/vendor/etc/wifi
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -147,7 +147,7 @@ LOCAL_MODULE       := p2p_supplicant_overlay.conf
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := $(LOCAL_MODULE)
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/wifi
+LOCAL_MODULE_PATH  := $(PRODUCT_OUT)/vendor/etc/wifi
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
